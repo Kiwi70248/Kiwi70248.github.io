f543ef26a4a22f1cb68a9fef9ac739f28df3fe66
Kiwi70248 <58341446+Kiwi70248@users.noreply.github.com>
2020-10-03 12:15:42 +0300

    Add files via upload

---
+++ back-grounds/birds/custom-back-ground.js
@@ -0,0 +1,19 @@
+VANTA.BIRDS({
+  el: "#custom-back-ground",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x20022,
+  color1: 0xffc200,
+  color2: 0x8600ff,
+  colorMode: "lerpGradient",
+  birdSize: 0.75,
+  wingSpan: 10.00,
+  speedLimit: 2.50,
+  separation: 100.00,
+  cohesion: 100.00
+})
